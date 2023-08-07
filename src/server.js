49ba2fb36cd7b3fcd057ccb0dd3f64745283d73b
BaoTranVQ <tranvanquocbao333@gmail.com>
2023-08-07 22:57:43 +0700

    routing and MVC video 8

--- src/server.js
+++ src/server.js
@@ -1,20 +1,19 @@
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+import initWebRoute from './route/web';
 
-import express from "express";
-import configViewEngine from "./configs/viewEngine";
-require('dotenv').config()
+require('dotenv').config();
 
-const app = express()
-const port = process.env.PORT;
-console.log("check Port:", port);
+const app = express();
+const port = process.env.PORT || 8080;
+
 configViewEngine(app);
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
 
-app.get('/about', (req, res) => {
-    res.send('Iam Bao ')
-  })
+
+//init web route
+initWebRoute(app);
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
